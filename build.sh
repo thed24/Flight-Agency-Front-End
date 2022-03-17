2b6b576c80d3b9f91d7a2279b252da4b0cc90ce3
Dom <domcodespoti@gmail.com>
2022-03-17 22:16:31 +1100

    wrap command in parenths

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 REGION=asia-southeast2
 PROJECT_ID=flight-agency-api
 
-API_URL=gcloud run services describe $PROJECT_ID --region $REGION --format 'value(status.url)'
+API_URL=$(gcloud run services describe $PROJECT_ID --region $REGION --format 'value(status.url)')
 echo $API_URL > /workspace/api_key
