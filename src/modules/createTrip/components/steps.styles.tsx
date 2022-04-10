be788259f0b3b717540b44dd7b7c1383b0262bf0
Dom <domcodespoti@gmail.com>
2022-04-10 11:46:45 +1000

    im a css genius

--- src/modules/createTrip/components/steps.styles.tsx
+++ src/modules/createTrip/components/steps.styles.tsx
@@ -1,14 +1,17 @@
 import styled from "@emotion/styled";
 
 export const Map = styled.div`
-  height: 400px;
-  width: 400px;
+  height: 600px;
+  width: 800px;
 `;
 
 export const MapContainer = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   gap: 10px;
   margin: 0 auto 10px;
+  justify-content: center;
 `;
