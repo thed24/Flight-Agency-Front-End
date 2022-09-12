330ee2cd75ae8c318a6189e758f0ae73d8465fa0
Dom <domcodespoti@gmail.com>
2022-09-12 16:57:40 +1000

    Remove side-effects to prevent barrel files being included in bundle

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-param-reassign */
 /**
  * @type {import('next').NextConfig}
  * */
 module.exports = {
     reactStrictMode: true,
+    swcMinify: true,
     experimental: {
         modularizeImports: {
             '@mui/material': {
@@ -12,6 +14,17 @@ module.exports = {
                 transform: '@mui/icons-material/{{member}}',
             },
         },
+    },
+    webpack: (config) => {
+        config.module.rules = [
+            ...config.module.rules,
+            {
+                test: /libs\/.*src\/index.ts/i,
+                sideEffects: false,
+            },
+        ]
+
+        return config;
     },
     compiler: {
         styledComponents: true,
--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,9 @@
-import type { DefaultUser } from 'next-auth';
+import type { NextComponentType, NextPageContext } from 'next';
+import type { Router } from 'next/router';
+import type { DefaultUser, Session } from 'next-auth';
 
 declare module 'next-auth' {
+    // eslint-disable-next-line no-shadow
     interface Session {
         user?: DefaultUser & {
             id: string;
@@ -18,4 +21,17 @@ declare module 'google-map-react' {
     interface Props {
         children?: any;
     }
+}
+
+declare module 'next/app' {
+    type AppProps<P = Record<string, unknown>> = {
+        Component: NextComponentType<NextPageContext, any, P>;
+        router: Router;
+        __N_SSG?: boolean;
+        __N_SSP?: boolean;
+        pageProps: P & {
+            /** Initial session passed in from `getServerSideProps` or `getInitialProps` */
+            session?: Session;
+        };
+    };
 }
